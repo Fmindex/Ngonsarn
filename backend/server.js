d332ca0cc54480137b7f2eacc1592c18902b413e
Kosate Limpongsa <kosatelim@gmail.com>
2017-06-20 21:16:50 +0700

    Move port 3000 to 3001

--- backend/server.js
+++ backend/server.js
@@ -53,8 +53,8 @@ app.put('/ngonsarn/:id', function(req, res){
 		});
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(3001);
+console.log("Server running on port 3001");
 
 /*
 {
