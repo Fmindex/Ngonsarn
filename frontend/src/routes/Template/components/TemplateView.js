6710faac877e75beb1f02c27dfa261b7fd037dc2
icesukhatouch <sukhatouchdenkirati@Sukhatouchs-MacBook-Pro.local>
2017-06-21 19:26:32 +0700

    remove global style

--- frontend/src/routes/Template/components/TemplateView.js
+++ frontend/src/routes/Template/components/TemplateView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
-import DuckImage from '../assets/Duck.jpg'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Jumbotron,Button } from 'reactstrap'
 import './TemplateView.scss'
+
 
 class TemplateView extends Component {
   constructor(props) {
@@ -31,20 +31,42 @@ class TemplateView extends Component {
   render() {
     const show = true;
     return (
-    <Navbar color="faded" light>
-          <NavbarToggler onClick={this.toggleNavbar} />
-          <Collapse className="navbar-toggleable-md" isOpen={!this.state.collapsed}>
-            <NavbarBrand href="/">reactstrap</NavbarBrand>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+<div>
+    <body className="test">
+          <Navbar light>
+            <NavbarToggler onClick={this.toggleNavbar} className="toggler" />
+            <Collapse className="navbar-toggleable-md" isOpen={!this.state.collapsed}>
+              <NavbarBrand href="/" className="myNav" >Ngonsarn</NavbarBrand>
+              <Nav className="myNav" navbar>
+                <NavItem>
+                  <NavLink href="/components/">งานสอน</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="">งอนสาน</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/components/">kuykuy</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/components/">About Us</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        
+          <Jumbotron>
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-2" />
+          <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
+        </Jumbotron>
+        
+
+    </body>
+    </div>
     )
   }
 }
